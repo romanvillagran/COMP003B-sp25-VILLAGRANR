2dfab5bb3b2b2096062f3a82622662be35db89a4
cbig3 <romanvillagran386@gmail.com>
2025-05-12 18:10:04 -0700

    added to appointment

--- COMP003B-sp25-VILLAGRANR/Models/Appointment.cs
+++ COMP003B-sp25-VILLAGRANR/Models/Appointment.cs
@@ -1,7 +1,20 @@
-﻿namespace COMP003B_sp25_VILLAGRANR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B_sp25_VILLAGRANR.Models
 {
     public class Appointment
     {
+        public int Id { get; set; }
 
+        [Required]
+        public DateTime AppointmentDate { get; set; }
+
+        [Required, StringLength(500)]
+        public string IssueDescription { get; set; }
+
+        public int VehicleId { get; set; }
+        public Vehicle Vehicle { get; set; }
+
+        public int ServiceId { get; set; }
     }
 }
