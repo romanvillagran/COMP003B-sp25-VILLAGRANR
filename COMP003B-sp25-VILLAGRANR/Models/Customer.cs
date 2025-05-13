73dbeb430e0c752fcf0ab5369cbe861e4fd060aa
cbig3 <romanvillagran386@gmail.com>
2025-05-12 17:55:02 -0700

    added to customer

--- COMP003B-sp25-VILLAGRANR/Models/Customer.cs
+++ COMP003B-sp25-VILLAGRANR/Models/Customer.cs
@@ -1,6 +1,20 @@
-﻿namespace COMP003B_sp25_VILLAGRANR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B_sp25_VILLAGRANR.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, StringLength(200)]
+        public string Address { get; set; }
+
+        [Required, Phone]
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Vehicle> Vehicles { get; set; }
     }
 }
