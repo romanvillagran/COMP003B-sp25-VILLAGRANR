2fbc3d11d5e6e050d0916ae826b2a12459c99159
cbig3 <romanvillagran386@gmail.com>
2025-05-12 19:52:13 -0700

    added the jason and the db to programs

--- COMP003B-sp25-VILLAGRANR/Program.cs
+++ COMP003B-sp25-VILLAGRANR/Program.cs
@@ -1,3 +1,6 @@
+using COMP003B_sp25_VILLAGRANR.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace COMP003B_sp25_VILLAGRANR
 {
     public class Program
@@ -8,6 +11,11 @@ namespace COMP003B_sp25_VILLAGRANR
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
             var app = builder.Build();
 
