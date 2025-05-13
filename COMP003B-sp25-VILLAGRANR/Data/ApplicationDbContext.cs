ac2bbf4eb64da302c9d87d8ee1e7b9510981b301
cbig3 <romanvillagran386@gmail.com>
2025-05-12 19:11:58 -0700

    added content to the db file

--- COMP003B-sp25-VILLAGRANR/Data/ApplicationDbContext.cs
+++ COMP003B-sp25-VILLAGRANR/Data/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace COMP003B_sp25_VILLAGRANR.Data
+﻿using COMP003B_sp25_VILLAGRANR.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B_sp25_VILLAGRANR.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
     }
 }
