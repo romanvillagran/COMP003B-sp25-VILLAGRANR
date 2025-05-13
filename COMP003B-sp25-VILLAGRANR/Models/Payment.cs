b25c37b72a4d916e8d77fbab18e3e6bbfa8b9897
cbig3 <romanvillagran386@gmail.com>
2025-05-12 19:36:04 -0700

    added a payment file and added content

---
+++ COMP003B-sp25-VILLAGRANR/Models/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B_sp25_VILLAGRANR.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int AppointmentId { get; set; }
+        public Appointment Appointment { get; set; }
+
+        [Required]
+        [Range(0, 10000)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; }
+    }
+}
