5b67801e5a1424109edd4aae8ef45365028db621
cbig3 <romanvillagran386@gmail.com>
2025-05-12 18:29:34 -0700

    restarting

--- COMP003B-sp25-VILLAGRANR/Models/Customer.cs
+++ COMP003B-sp25-VILLAGRANR/Models/Customer.cs
@@ -4,17 +4,6 @@ namespace COMP003B_sp25_VILLAGRANR.Models
 {
     public class Customer
     {
-        public int Id { get; set; }
-
-        [Required, StringLength(100)]
-        public string Name { get; set; }
-
-        [Required, StringLength(200)]
-        public string Address { get; set; }
-
-        [Required, Phone]
-        public string PhoneNumber { get; set; }
-
-        public ICollection<Vehicle> Vehicles { get; set; }
+    
     }
 }
