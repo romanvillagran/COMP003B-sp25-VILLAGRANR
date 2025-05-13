b74025ca4afa9b643c176f4b69a35dd1d28e7671
cbig3 <romanvillagran386@gmail.com>
2025-05-12 17:59:01 -0700

    added to vehicle

--- COMP003B-sp25-VILLAGRANR/Models/Vehicle.cs
+++ COMP003B-sp25-VILLAGRANR/Models/Vehicle.cs
@@ -1,6 +1,26 @@
-﻿namespace COMP003B_sp25_VILLAGRANR.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B_sp25_VILLAGRANR.Models
 {
     public class Vehicle
     {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Make { get; set; }
+
+        [Required, StringLength(50)]
+        public string Model { get; set; }
+
+        [Required, Range(1900, 2100)]
+        public int Year { get; set; }
+
+        [Required, StringLength(20)]
+        public string LicensePlate { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
